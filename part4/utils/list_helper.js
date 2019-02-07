75e7559f62c2f4cbecfa22d8755b81e9e1757b7a
koivkai <kaius.koivunen@gmail.com>
2019-02-07 20:48:12 +0200

    part4 ex 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,18 @@
 const dummy = (blogs) => {
     return 1
-  }
+}
+
+const totalLikes = (blogs) => {
+    if(blogs.length === 0) {
+        return 0
+    }
+    let total = blogs.reduce((sum, nextValue) => {
+        return sum + nextValue.likes
+    },0)
+    return total
+}
   
-  module.exports = {
-    dummy
-  }
+module.exports = {
+    dummy,
+    totalLikes
+}
