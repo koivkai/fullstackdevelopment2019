ea0c2e4402a4ca9ce3ef48a5f63a71747409cc5e
koivkai <kaius.koivunen@gmail.com>
2019-02-07 21:27:34 +0200

    part4 ex 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -11,8 +11,27 @@ const totalLikes = (blogs) => {
     },0)
     return total
 }
+
+const favoriteBlog = (blogs) => {
+    let first = blogs[0]
+    //console.log('first', first)
+
+    let favorite = blogs.reduce((favSoFar, current) => {
+        //console.log('favSoFar', favSoFar, 'current', current)
+        //console.log('current likes', current.likes, 'favsofar likes ', favSoFar.likes)
+        if (current.likes > favSoFar.likes) {
+            //console.log('IFFIFIFIFIFIFIFIFIFIFIFFIFIIFFIFI')
+            return current           
+        }
+        //console.log('iffin jälkeen favsofar', favSoFar)
+        return favSoFar
+    }, first)
+    //console.log('Favorite to return', favorite)
+    return favorite
+}
   
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
