b880c344a45c133e93dc819cbf504d3129fa1990
koivkai <kaius.koivunen@gmail.com>
2019-02-11 23:12:44 +0200

    part4 working version of ex 4.15

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -3,10 +3,8 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User
-      .find({}).populate('blogs')
-  
-    response.json(users.map(User.format))
+    const users = await User.find({})
+    response.json(users.map(u => u.toJSON()))
   })
 
 usersRouter.post('/', async (request, response) => {
