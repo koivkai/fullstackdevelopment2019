e4da51696126318f839aa3a508b6e986eb62b87c
koivkai <kaius.koivunen@gmail.com>
2019-02-15 19:46:06 +0200

    part4 ex 4.17 4.18 4.19

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,8 +1,18 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
   //console.log('blogs returned', blogs)
   response.json(blogs)
 })
@@ -11,19 +21,47 @@ blogsRouter.get('/', async (request, response) => {
     const blog = new Blog(request.body)
     //console.log('request body', request.body)
 
-    const blogToAdd = new Blog({
-      title: blog.title,
-        author: blog.author === undefined ? '' : blog.author,
-        url: blog.url,
-        likes: blog.likes === undefined ? 0 : blog.likes
-    })
+    const token = getTokenFrom(request)
+    console.log('token', token)
+    
 
     try {
+      if(!token) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+      }
+      const decodedToken = jwt.verify(token, process.env.SECRET)
+      if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+      }
+      console.log('decotedToken', decodedToken)
+      console.log('decodedToken.id', decodedToken.id)
+      const user = await User.findById(decodedToken.id)
+      console.log('user', user)
+      const blogToAdd = new Blog({
+        title: blog.title,
+        author: blog.author === undefined ? '' : blog.author,
+        url: blog.url,
+        likes: blog.likes === undefined ? 0 : blog.likes,
+        user: user._id
+      })    
+  
+      const savedBlog = await blogToAdd.save()
+      user.blogs = user.blogs.concat(savedBlog._id)
+      await user.save()
+      response.status(201).json(savedBlog.toJSON())
+      
+    } catch(error) {
+      response.status(400).json({error: error.message})
+    }
+
+    
+
+   /*  try {
       const savedBlog = await blogToAdd.save()
       response.status(201).json(savedBlog.toJSON())
     } catch (error) {
       response.status(400).json({error: error.message})
-    }
+    } */
 
     /* blog
       .save()
