c401bd0a65d5fe16a9b6ac0b2ad0f2685f59147a
koivkai <kaius.koivunen@gmail.com>
2019-02-25 14:39:18 +0200

    part5 ex 5.14

--- part5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ part5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'jest-dom/extend-expect'
-import { render, cleanup } from 'react-testing-library'
+import { render, cleanup, fireEvent } from 'react-testing-library'
 import SimpleBlog from './SimpleBlog'
 
 afterEach(cleanup)
@@ -15,7 +15,7 @@ test('blog is rendered correctly', () => {
 
     const component = render(
         <SimpleBlog blog={blog}/>
-    )
+    ) 
 
     expect(component.container).toHaveTextContent(
         'Batmans batblog batman'
@@ -25,4 +25,31 @@ test('blog is rendered correctly', () => {
     expect(likesDiv).toHaveTextContent(
         'blog has 2 likes'
     )
+})
+
+test('pressing like button twice calls eventhandler twice', () => {
+    const blog = {
+        title: 'Batmans batblog',
+        author: 'batman',
+        url: 'www.batcave/bats/blog',
+        likes: 2
+    }
+
+    const mockLikeHandler = jest.fn()
+
+    const { getByText }= render(
+        <SimpleBlog blog={blog} onClick={mockLikeHandler}/>
+    )
+
+    //component.debug()
+
+    const likeButton = getByText('like')
+
+    //console.log('button', likeButton, 'typeof,', typeof(likeButton))
+
+    fireEvent.click(likeButton)
+    expect(mockLikeHandler.mock.calls.length).toBe(1)
+
+    fireEvent.click(likeButton)
+    expect(mockLikeHandler.mock.calls.length).toBe(2)
 })
